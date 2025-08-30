83c41dbda1f3f650e8144e62283849e124db5f19
m.furqan <furqan123arain@gmail.com>
2025-08-30 16:42:29 +0500

    one more url added update

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const app = express();
 const allowedOrigins = [
   "http://localhost:3000",
   "http://127.0.0.1:5500",
+  "http://127.0.0.1:5500/",
   "https://siteareahyderabadfrontend.vercel.app" 
 ];
 app.use( cors({
