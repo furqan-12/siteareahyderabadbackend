7dc9378147a43c3f69675f203f849c1bc9d9ac87
m.furqan <furqan123arain@gmail.com>
2025-10-03 09:58:48 +0500

    last upadte related to cors error

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 const allowedOrigins = [
-  "http://localhost:3000",
   "http://127.0.0.1:5500",
+  "http://127.0.0.1:5501/",
   "https://siteareahyderabadfrontend.vercel.app"
 ];
 
